b7285e36976a5ef674accf8885ca92dc414e97de
venkateshpalagiri <venkateshpalagiri@gmail.com>
2024-03-03 22:09:12 -0500

    Project setup

---
+++ src/main/java/com/venkatesh/SpringBootPlayGround/SpringBootPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package com.venkatesh.SpringBootPlayGround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlayGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlayGroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venkatesh/SpringBootPlayGround/SpringBootPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatesh.SpringBootPlayGround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlayGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
