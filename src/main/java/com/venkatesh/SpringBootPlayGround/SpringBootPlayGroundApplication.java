b30131771416341a631c03d3f8d6e0c6061bdc25
venkateshpalagiri <venkateshpalagiri@gmail.com>
2024-03-05 22:00:00 -0500

    reg commit

--- src/main/java/com/venkatesh/SpringBootPlayGround/SpringBootPlayGroundApplication.java
+++ src/main/java/com/venkatesh/SpringBootPlayGround/SpringBootPlayGroundApplication.java
@@ -1,9 +1,11 @@
 package com.venkatesh.SpringBootPlayGround;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SpringBootPlayGroundApplication {
 
 	public static void main(String[] args) {
